3157fdea4f9163ee1fc719e7b814527ad067f65d
timilak <52374157+timilak@users.noreply.github.com>
2020-07-31 11:19:06 -0700

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,18 @@
+# concatenating strings
+mystring = "Hello World!"
+string1 = " I'm 17 years old"
+print(mystring + string1)
+
+#slicing strings: this prints the first 5 characters of the string
+sliced_string = mystring[:5]
+print(sliced_string)
+
+integer = 5
+floating_number = 50075.95
+string = "45"
+
+# convert string to float
+new_float = float(string)
+
+print(type(new_float))
+print(new_float)
