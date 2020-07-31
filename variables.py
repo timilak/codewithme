4165f1424dc98dfb41b8cf6b993e1be254545b6b
timilak <52374157+timilak@users.noreply.github.com>
2020-07-31 11:20:29 -0700

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,11 @@
+x = y = z = "Apple"
+
+file = open("mysfirstfile.txt", "w")
+
+file.write("Hello! This is Timila writing to the file") 
+
+file.close()
+
+# create your own file, write something in it.
+# open up this file, read the contents and print them out.
+# read: r  write: w; append: a; create: x,
