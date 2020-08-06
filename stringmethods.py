0feb7b3176f9d423ca4e7565f7f77fa262d377b1
timilak <52374157+timilak@users.noreply.github.com>
2020-08-06 08:02:27 -0700

    Update stringmethods.py

--- stringmethods.py
+++ stringmethods.py
@@ -2,26 +2,26 @@
 mystring = "Hello there! Is there anyone here? There is no one here."
 upper_string = mystring.upper()
 lower_string = mystring.lower()
-# print(lower_string)
+print(lower_string)
 
 # len function
 # print(len(mystring))
 
 # count method
 count_here = lower_string.count("here", 5, 21)
-#print(count_here)
+print(count_here)
 
 # find method
 find_string = mystring.rfind("here")
-#print(find_string)
+print(find_string)
 
 # replace
 replaced = mystring.replace("e", "a", 3)
-#print(replaced)
+print(replaced)
 
 # split
 split_string = mystring.split()
-#print(split_string)
+print(split_string)
 
 fruits = ["Apple", "Cherry", "Orange"]
 
