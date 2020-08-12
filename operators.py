3300f3e31965c7fc95d4ca39af85cee959b9cb49
timilak <52374157+timilak@users.noreply.github.com>
2020-08-12 12:03:03 -0700

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,23 @@
+# This is a comment! Python ignores it!
+# This piece of code is about python operators
+
+# arithmetic operators: +, -, *, /, **
+a = 15
+b = 6
+print (a+b)
+print (a-b)
+print (a*b)
+print (a/b)         #remainder
+print (a**b)        #exponent
+
+# logical operators: >, <, >=, <=, ==, !=
+print (b!=a)         # not equal
+print (b==a)
+print (b>a)
+print (b<a)
+print (b>=a)        # greater than or equal
+print (b<=a)        # less than or equal
+
+# output will be True or False (Boolean)
+
+# logical operators: and , or, not
