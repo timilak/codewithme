b06d87560a0ee2e374e913a141d01980c6c5fe00
timilak <52374157+timilak@users.noreply.github.com>
2020-08-13 21:44:15 -0700

    Add files via upload

---
+++ whileloop.py
@@ -0,0 +1,16 @@
+# testing out the while loop
+
+i = 2
+while i<=10:
+    print(i)
+    i += 1      #remove this line to make an infinite loop
+# press 'Ctrl' and 'C' to get out of infinite loop
+
+# find the sum of all numbers till 20
+
+sum = 0
+num = 0
+while num<=20:
+    sum += num
+    num += 1
+print(sum)
